518d08cbd6404b98cbb01fc54f47d6c492a60ede
João Paulo <costajoaopaulo113@gmail.com>
2023-04-02 01:39:35 -0300

    primeira parte

---
+++ Cliente/src/service/ClienteService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import bean.ChatMessage;
+
+/**
+ *
+ * @author João P. Arquim
+ */
+public class ClienteService {
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect(){
+        try {
+            socket = new Socket("localhost", 5555);
+            output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return socket;
+    }
+    
+    public void send(ChatMessage message){
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
